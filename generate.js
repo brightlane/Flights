eebce0c2de57a9535ae97b0030f6f9e600fe30bd
brightlane <benitopalmarini@aol.com>
2026-04-15 08:01:08 -0400

    Update generate.js

--- generate.js
+++ generate.js
@@ -52,3 +52,36 @@ if (!fs.existsSync(folder)) {
 fs.writeFileSync(`${folder}/${fileName}`, generateArticle(topic));
 
 console.log("Generated:", fileName);
+const fs = require("fs");
+
+const baseUrl = "https://brightlane.github.io/Flights";
+
+const postsDir = "./posts";
+
+const files = fs.readdirSync(postsDir);
+
+let urls = files.map(file => {
+    return `
+  <url>
+    <loc>${baseUrl}/posts/${file}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>`;
+}).join("\n");
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>${baseUrl}/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+${urls}
+
+</urlset>`;
+
+fs.writeFileSync("./sitemap.xml", sitemap);
+
+console.log("Sitemap generated");
